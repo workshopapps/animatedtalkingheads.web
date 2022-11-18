a4192a1ac6d1fd97184f3fd838fb5fa4544767ff
Babajide Akinseye <bakinseye247@gmail.com>
2022-11-18 16:50:02 +0100

    chore(FRO-33):modify nav test

---
+++ src/libs/links.js
@@ -0,0 +1,7 @@
+export const links = [
+  { name: "Home", link: "/"},
+  { name: "About Us", link: "/about"},
+  { name: "Use cases", link: "/use-cases"},
+  { name: "Contact", link: "/contact"},
+  { name: "How it work", link: "/how-it-works"},
+]
