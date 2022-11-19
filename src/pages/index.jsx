3d51243c0ee5822261abf3e753c66c31ef594169
Emmanuel Jemeni <jemenichinonso11@gmail.com>
2022-11-19 17:35:21 +0100

    feat: added in-app chat functionality

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -1,7 +1,15 @@
 import Layout from '../components/UI/Layout';
 
+// <>
+//   <button onClick={() => setShow((prevState) => !prevState)}>Chat</button>
+//   <ChatBox isChatBoxShowing={show} setIsChatBoxShowing={setShow} />
+// </>
+
 const Home = () => {
-  return <Layout></Layout>;
+  return (
+    <Layout>
+    </Layout>
+  );
 };
 
 export default Home;
