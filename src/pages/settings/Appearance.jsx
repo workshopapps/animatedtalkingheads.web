0fabedbf5f560defbe86136d5aa213f385eda3f2
Sheriff Amzat <kunmiamzat1@gmail.com>
2022-11-19 18:04:19 +0100

    Fixed responsiveness of top section

--- src/pages/settings/Appearance.jsx
+++ src/pages/settings/Appearance.jsx
@@ -2,20 +2,20 @@ import { ReactComponent as Brush } from "./assets/brush.svg"
 
 const Appearance = () => {
     return (
-        <section className="mt-8 ml-2 w-[50vw] border-b-[#D9D9D9] border-b pb-4">
+        <section className="mt-8 max-w-[460px] border-b-[#D9D9D9] border-b pb-4 ">
             <header className="flex mb-6">
                 <Brush />
                 <p className="text-[#2563EB]">Appearance</p>
             </header>
-            <div className="flex flex-wrap gap-x-10">
-                <div className="border-b-[#D9D9D9] border-b w-[150px] pb-4">
+            <div className="flex flex-wrap gap-x-20 lg:gap-x-36">
+                <div className="border-b-[#D9D9D9] border-b w-[150px] pb-4 mb-4">
                     <p className="text-sm">Theme</p>
                     <select className="text-xs">
                         <option>Light</option>
                         <option>Dark</option>
                     </select>
                 </div>
-                <div className="border-b-[#D9D9D9] border-b w-[150px] pb-4">
+                <div className="border-b-[#D9D9D9] border-b w-[150px] pb-4 mb-4">
                     <p className="text-sm">Font Size</p>
                     <select className="text-xs">
                         <option>Small(Recommended)</option>
@@ -23,21 +23,21 @@ const Appearance = () => {
                         <option>Big</option>
                     </select>
                 </div>
-                <div className="border-b-[#D9D9D9] border-b w-[150px] pb-4">
+                <div className="border-b-[#D9D9D9] border-b w-[150px] pb-4 mb-4">
                     <p className="text-sm">Language</p>
                     <select className="text-xs">
                         <option>English [en-US)</option>
                         <option>English [en-CA)</option>
                     </select>
                 </div>
-                <div className="border-b-[#D9D9D9] border-b w-[150px] pb-4">
+                <div className="border-b-[#D9D9D9] border-b w-[150px] pb-4 mb-4">
                     <p className="text-sm">Video Template</p>
                     <select className="text-xs">
                         <option>Default</option>
                         <option>Recommended</option>
                     </select>
                 </div>
-                <div className="border-b-[#D9D9D9] border-b w-[150px] pb-4">
+                <div className="border-b-[#D9D9D9] border-b w-[150px] pb-4 mb-4">
                     <p className="text-sm">Intro Music</p>
                     <select className="text-xs">
                         <option>None</option>
@@ -45,7 +45,7 @@ const Appearance = () => {
                     </select>
                 </div>
             </div>
-            <button type="button" className="bg-[#2563EB] px-3 h-14 w-28 rounded-md mt-8">Save</button>
+            <button type="button" className="bg-[#2563EB] px-3 h-14 w-28 rounded-md mt-8 right-0">Save</button>
         </section>
     )
 }
--- src/pages/settings/index.jsx
+++ src/pages/settings/index.jsx
@@ -5,9 +5,9 @@ import RightSection from './RightSection';
 const Setting = () => {
   return (
     <Layout>
-      <main className='w-[85vw] m-auto'>
+      <main className='w-[75vw] m-auto'>
         <header className="text-3xl capitalize mt-8">Settings</header>
-        <div className="grid sm:flex sm:justify-between">
+        <div className="grid md:flex md:justify-between ml-8">
           <LeftSection />
           <RightSection />
         </div>
