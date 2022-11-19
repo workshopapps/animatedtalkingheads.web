a2773221e871da705f5b3d82a4c5efce3bb4308a
Sheriff Amzat <kunmiamzat1@gmail.com>
2022-11-20 00:31:38 +0100

    Fixed some styles

--- src/pages/settings/components/Accessibility.jsx
+++ src/pages/settings/components/Accessibility.jsx
@@ -1,17 +1,19 @@
-import { ReactComponent as Accesibility } from "./assets/Accessibility.svg";
+import { ReactComponent as Accesibility } from "../assets/Access.svg";
+import ToggleSwitch from "./ToggleSwitch";
 
 const Accessibility = () => {
     return (
         <section>
             <header className="flex mb-6 mt-6">
                 <Accesibility />
-                <p className="text-[#2563EB]">Accesibility</p>
+                <p className="text-[#2563EB]">Accessibility</p>
             </header>
             <div className="grid gap-3">
                 <div className="border-b border-b-[#D9D9D9] pb-4">
                     <p className="text-md">Caption</p>
-                    <div>
+                    <div className="flex gap-72">
                         <p className="text-xs">Live caption</p>
+                        <ToggleSwitch />
                     </div>
                 </div>
                 <div className="border-b border-b-[#D9D9D9] pb-4">
@@ -24,7 +26,7 @@ const Accessibility = () => {
                     </div>
                     <div className="flex justify-between">
                         <p className="text-xs">Automatically refresh feed for new posts</p>
-                        <button type="button" className="bg-[#2563EB] px-3 h-10 w-28 rounded">Save</button>
+                        <button type="button" className="bg-[#2563EB] px-3 h-10 w-28 rounded text-white">Save</button>
                     </div>
                 </div>
             </div>
