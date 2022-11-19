a579d6d6b934e63d56ecfdf69883146672d6868e
unknown <lateefjeremiah@gmail.com>
2022-11-19 05:11:07 +0100

    styles: added screen sizes to the global style

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,6 +3,12 @@
 module.exports = {
   content: ['./src/**/*.{js,jsx,ts,tsx}'],
   theme: {
+    screens: {
+      sm: '480px',
+      md: '768px',
+      lg: '976px',
+      xl: '1440px'
+    },
     extend: {
       colors: {
         pri: '#2158D2',
