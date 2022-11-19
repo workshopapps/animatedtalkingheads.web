e36a81a2f005eead97d07f8803229a03ea3c38b6
bobbycrooz <idrisloove@gmial.com>
2022-11-19 17:52:01 +0100

    fixed global styling issues in careers page

--- src/pages/careers/index.jsx
+++ src/pages/careers/index.jsx
@@ -2,7 +2,7 @@ import Layout from '../../components/UI/Layout';
 import Hero from './Hero/Hero';
 import Meet from './meet/Meet';
 import Future from './future/Future';
-import './index.modules.scss';
+// import './index.modules.scss';
 
 const Careers = () => {
   return (
