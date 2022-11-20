c42b46bbd341043c2a0ecf0d290bcf92f86debf7
ifeday1 <oladeniifeoluwa123@gmail.com>
2022-11-20 23:08:51 +0100

    Update

--- src/pages/settings/IntegrationAndApps.jsx
+++ src/pages/settings/IntegrationAndApps.jsx
@@ -1,3 +1,4 @@
+import React from "react";
 import { ReactComponent as Search } from "./assets/search-normal.svg";
 import { ReactComponent as YouTube } from "./assets/youtube.svg";
 import { ReactComponent as Twitter } from "./assets/twitter.svg";
@@ -8,7 +9,11 @@ import { ReactComponent as DropBox } from "./assets/dropbox.svg";
 import { ReactComponent as Instagram } from "./assets/instagram.svg";
 import { ReactComponent as LinkedIn } from "./assets/linkedin.svg";
 
+
 const IntegrationAndApps = () => {
+    const [errorMessage, setErrorMessage] = useState('')
+
+    const handleClick = () => setErrorMessage('Could not connect app successfully. Try again')
     return (
         <section className="mt-6">
             <header className="flex mb-6">
@@ -18,10 +23,14 @@ const IntegrationAndApps = () => {
             <div className="flex gap-4 mt-6">
                 <p className="text-sm self-center">Connect VoxClips to</p>
                 <div className="relative flex">
-                    <Search className="absolute"/>
-                    <input type="search" className="bg-[#949698] h-14" placeholder="Search for Apps"/>
+                    <Search className="absolute" />
+                    <input type="search" className="bg-[#949698] h-14" placeholder="Search for Apps" />
                 </div>
-                <button className="bg-[#2563EB] px-3 h-14 w-24 rounded-md text-white">Connect</button>
+                <div>
+                    <button onClick={handleClick} className="bg-[#2563EB] px-3 h-14 w-24 rounded-md text-white">Connect</button>
+                    {errorMessage}
+                </div>
+
             </div>
             <div className="flex gap-x-20 mt-6">
                 <div className="grid gap-y-5">
@@ -37,51 +46,51 @@ const IntegrationAndApps = () => {
                             <Twitter />
                             <p>Twitter</p>
                         </div>
-                        
+
                     </div>
                     <div className="border-t pt-4">
                         <div className="flex gap-3">
                             <FaceBook />
                             <p>FaceBook</p>
                         </div>
-                        
+
                     </div>
                     <div className="border-t pt-4 border-b pb-4">
                         <div className="flex gap-3">
                             <Gdrive />
                             <p>Google Drive</p>
                         </div>
-                        
+
                     </div>
                 </div>
                 <div className="grid gap-y-5">
-                <div className="border-t pt-4">
-                    <div className="flex gap-3">
+                    <div className="border-t pt-4">
+                        <div className="flex gap-3">
                             <OneDrive />
                             <p>One Drive</p>
                         </div>
-                        
+
                     </div>
                     <div className="border-t pt-4">
                         <div className="flex gap-3">
                             <DropBox />
                             <p>Drop Box</p>
                         </div>
-                        
-                    </div>                    
+
+                    </div>
                     <div className="border-t pt-4">
                         <div className="flex gap-3">
                             <Instagram />
                             <p>Instagram</p>
                         </div>
-                        
-                    </div>                    
+
+                    </div>
                     <div className="border-t pt-4 border-b pb-4">
                         <div className="flex gap-3">
                             <LinkedIn />
                             <p>LinkedIn</p>
                         </div>
-                        
+
                     </div>
 
                 </div>
