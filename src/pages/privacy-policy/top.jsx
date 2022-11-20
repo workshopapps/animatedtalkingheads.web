8c84a8770493025ac8bbcbac4f52429a63d00a37
just-hope-netizen <hopepeter007@gmail.com>
2022-11-20 15:47:11 +0100

    fix 12: grammar errors

--- src/pages/privacy-policy/top.jsx
+++ src/pages/privacy-policy/top.jsx
@@ -2,7 +2,10 @@
 const Top = () => {
     return (
       <div className="bg-pri pt-8 h-[251px]">
-        <p className={"text-white !font-medium text-[25px] lg:text-4xl  text-center"} > Privacy Policy</p>
+        <p className={'text-white !font-medium text-[25px] lg:text-4xl  text-center'}>
+          {' '}
+          Privacy Policy
+        </p>
       </div>
     );
 }
