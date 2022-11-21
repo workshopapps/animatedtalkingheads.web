d61520d33de977b96762e27172b3257ea70aec90
Sheriff Amzat <kunmiamzat1@gmail.com>
2022-11-21 21:36:14 +0100

    changed css to module css

--- src/pages/settings/components/ToggleSwitch.jsx
+++ src/pages/settings/components/ToggleSwitch.jsx
@@ -1,13 +1,13 @@
 import React, { useState } from "react";
-import "./ToggleSwitch.css";
+import css from "./ToggleSwitch.module.css";
 
 const ToggleSwitch = ({ toggledState }) => {
   const [isToggled, setIsToggled] = useState(toggledState);
   const onToggle = () => setIsToggled(!isToggled);
   return (
-    <label className="toggle-switch">
-      <input type="checkbox" checked={isToggled} onChange={onToggle} />
-      <span className="switch" />
+    <label className={css.toggleswitch}>
+      <input type="checkbox" className={css.input} checked={isToggled} onChange={onToggle} />
+      <span className={css.switch} />
     </label>
   );
 }
