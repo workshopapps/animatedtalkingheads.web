65babb9727a18b137dab3d232ccfd5c3423a0d83
Daniel Tolani <mumytosin2016@gmail.com>
2022-11-25 06:32:08 +0100

    feat(FRO-21): video play scrub handle active on hover

--- src/pages/podcast/download/index.jsx
+++ src/pages/podcast/download/index.jsx
@@ -100,7 +100,11 @@ const DownloadPodcast = () => {
               <div className={containerToggleClass()}>
                 {/* <iframe width="560" height="315" src="https://player.vimeo.com/video/499958263?h=06b4122553" title="VoxClips video player" allowFullScreen></iframe> */}
                 <div className='video-controls-container'>
-                   <div className='timeline-container'></div>
+                   <div className='timeline-container'>
+                    <div className='timeline'>
+                      <div className='thumb-indicator'></div>
+                    </div>
+                   </div>
                    <div className='controls'>
                     <button onClick={backForward}  className='rewind-button'><svg xmlns="http://www.w3.org/2000/svg" width="48" height="48" fill="none"><path stroke="#fff" strokeLinecap="round" strokeLinejoin="round" strokeWidth="1.5" d="M21.54 27.918v-5.34l-1.5 1.67M22.02 16.47 24 14"/><path stroke="#fff" strokeLinecap="round" strokeLinejoin="round" strokeWidth="1.5" d="M16.91 19.799c-1.11 1.48-1.8 3.31-1.8 5.31a8.89 8.89 0 0 0 8.89 8.89 8.89 8.89 0 0 0 8.89-8.89 8.89 8.89 0 0 0-8.89-8.89c-.68 0-1.34.09-1.98.24"/><path stroke="#fff" strokeLinecap="round" strokeLinejoin="round" strokeWidth="1.5" d="M26 22.578c1.1 0 2 .9 2 2v1.35c0 1.1-.9 2-2 2s-2-.9-2-2v-1.35a2 2 0 0 1 2-2Z"/></svg></button>
                     <button onClick={togglePlay} className='play-pause-button'>
