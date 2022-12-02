5dfffab10afa872844755135251e39346f3b402d
Joshua Evuetapha <Evuetaphajoshua@gmail.com>
2022-12-02 11:12:34 +0100

    fix: change icons in the avatar page

--- src/pages/avatar/index.jsx
+++ src/pages/avatar/index.jsx
@@ -1,60 +1,57 @@
 import React, { useState } from 'react';
 import Layout from '../../components/UI/Layout';
-import AVATAR_IMAGES from './avatar.data';
 import SIDE_IMAGES from './avatar-side.data';
-
 import avatarStyles from './avatar.module.css';
 import { Button } from '../../components/UI/Button';
+import AVATAR_SET_1 from './avatar.data.set-1';
+import AVATAR_SET_2 from './avatar.data.set-2';
+import { Link } from 'react-router-dom';
+import { routes } from '../../libs/links';
 
 const Avatars = () => {
-  const [avatars] = useState(AVATAR_IMAGES);
   const [sideAvatars] = useState(SIDE_IMAGES);
   return (
     <Layout>
-      <div className={avatarStyles.avatars_page}>
-        <div className={avatarStyles.header}>
-          <div className={avatarStyles.links}>
-            <a href="/" className={avatarStyles.link}>
-              Home
-            </a>
-            <span className={avatarStyles.gap}>&gt;</span>
-            <a href="/avatars" className={avatarStyles.link}>
-              Avatars
-            </a>
+      <div>
+
+        <div className={avatarStyles.top_bg}><h1>Avatar library</h1></div>
+        <h2 className={avatarStyles.heading}> Explore Our Avatars at VoxClips </h2>
+        <div className='flex justify-center mt-4'><p className={avatarStyles.info}>Meet our avatars and know how and when to use them</p></div>
+   
+
+        <div className='flex justify-center'>
+
+          <div className='px-40 max-w-[1532px]'>
+
+            <div className={avatarStyles.section}>
+              <div className={`${avatarStyles.avatars} ${avatarStyles.front}`}>
+                {AVATAR_SET_1.map(({ id, image }) => (
+                  <div className={avatarStyles.bckg} key={id}>
+                    <img src={image} alt="Avatar" className={avatarStyles.img} />
+                  </div>
+                ))}
+              </div>
+            </div>
+
+            <div className={avatarStyles.section}>
+              <div className={`${avatarStyles.avatars} ${avatarStyles.side}`}>
+                {AVATAR_SET_2.map(({ id, image }) => (
+                  <div className={avatarStyles.bckg} key={id}>
+                    <img src={image} alt="Side Facing Avatar" className={avatarStyles.img} />
+                  </div>
+                ))}
+              </div>
+            </div>
+
+
+            <div className='flex justify-center'>
+              <Link to={routes.scenery} className={avatarStyles.last_link}> Explore various environments and scenery {'>>'} </Link>
+            </div>
+
           </div>
 
-          <h2 className={avatarStyles.heading}>VoxClips Avatar Library</h2>
-          <p className={avatarStyles.info}>Meet our avatars and know how and when to use them</p>
         </div>
 
-        <div className={avatarStyles.section}>
-          <p className={avatarStyles.text}>
-            The front facing avatars are perfect for audios that have only one speaker.
-          </p>
-          <div className={`${avatarStyles.avatars} ${avatarStyles.front}`}>
-            {avatars.map(({ id, image }) => (
-              <div className={avatarStyles.bckg} key={id}>
-                <img src={image} alt="Avatar" className={avatarStyles.img} />
-              </div>
-            ))}
-          </div>
-        </div>
-        <div className={avatarStyles.section}>
-          <p className={avatarStyles.text}>
-            The side facing avatars are perfect for audios that have two speakers.
-          </p>
-          <div className={`${avatarStyles.avatars} ${avatarStyles.side}`}>
-            {sideAvatars.map(({ id, image }) => (
-              <div className={avatarStyles.bckg} key={id}>
-                <img src={image} alt="Side Facing Avatar" className={avatarStyles.img} />
-              </div>
-            ))}
-          </div>
-          <Button> Meet the Avatars</Button>
-          <a href="/avatar/scenery" className={avatarStyles.scenery}>
-            Explore our Various Environments and Scenery &gt;&gt;
-          </a>
-        </div>
       </div>
     </Layout>
   );
