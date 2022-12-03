7ea52599ffff5532e3fe6d5204df33d78ca4ddb2
Bitee-cd <Calebduniya45@gmail.com>
2022-12-03 18:29:11 +0100

    landing page implemetation

---
+++ src/components/pages/landing/Freestyle.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import arrowBack from '../../../assets/icons/free_style.svg';
+
+const Freestyle = () => {
+  return (
+    <section className="bg-sec-700 py-20">
+      <div className="mx-auto max-w-[1440px] w-[90%] grid md:grid-cols-2 text-white">
+        <div className="w-[80%] mx-auto ">
+          <img src={arrowBack} className="w-full" alt="images of characters" />
+        </div>
+        <div className="w-[80%] flex justify-center items-center mx-auto my-5">
+          <p className="font-bold text-lg lg:text-4xl text-center">
+            Freestyle with various diverse characters and share on any social platform
+          </p>
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default Freestyle;
--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -12,6 +12,9 @@ import Quotes from '../assets/icons/quotes.svg';
 import MarkImg from '../assets/images/mark-essien.png';
 import LavyImg from '../assets/images/lavy-engine.png';
 import NonsoImg from '../assets/images/Nonso.png';
+import Freestyle from '../components/pages/landing/Freestyle';
+import Transform from '../components/pages/landing/Transform';
+import StartCreating from '../components/pages/landing/StartCreating';
 // import { Button } from '../components/UI/Button';
 
 // import ChatBox from '../components/ChatBox';
@@ -95,85 +98,86 @@ const Home = () => {
           </div>
         </div>
       </section>{' '}
-      <section className="bg-white container mx-auto">
+      <section className="bg-[#F5F5F5] container mx-auto">
         <div className="pt-[60px] ">
-          <h3 className="text-center text-xl pb-12 md:pb-32 md:text-[40px] text-[#292d32] font-bold ">
+          <h3 className="text-center text-xl pb-12 md:pb-32 md:text-[40px] text-[#292d32]  ">
             4 Simple clicks gets you there
           </h3>
           <div className="md:flex justify-between items-center flex-wrap">
             <div className="mx-auto pb-5">
-              <h3 className="border border-solid border-black border-opacity-10 rounded-[50%] md:w-[74px] md:h-[74px] h-9 w-9   text-center grid items-center text-base md:text-[32px] mx-auto">
+              <h3 className="border shadow bg-white border-solid border-black border-opacity-10 rounded-[50%] md:w-[74px] md:h-[74px] h-9 w-9   text-center grid items-center text-base md:text-[32px] mx-auto">
                 1
               </h3>
-              <p className="mx-auto w-44 md:w-52  text-center md:font-bold md:text-2xl text-[14px] pt-3 md:pt-8">
-                Upload Your Audio Podcast (mp4, wav, m4a)
+              <p className="mx-auto w-44 md:w-52  text-center  md:text-2xl text-[14px] pt-3 md:pt-8">
+                Upload Your Audio Podcast
               </p>
             </div>
             <div className="mx-auto pb-5">
-              <h3 className="border border-solid border-black border-opacity-10 rounded-[50%] md:w-[74px] md:h-[74px] h-9 w-9   text-center grid items-center text-base md:text-[32px] mx-auto ">
+              <h3 className="border  bg-white border-solid border-black border-opacity-10 rounded-[50%] md:w-[74px] md:h-[74px] h-9 w-9   text-center grid items-center text-base md:text-[32px] mx-auto ">
                 2
               </h3>
-              <p className="mx-auto w-44 md:w-52  text-center md:font-bold md:text-2xl text-[14px] pt-3 md:pt-8">
+              <p className="mx-auto w-44 md:w-52  text-center  md:text-2xl text-[14px] pt-3 md:pt-8">
                 Choose Your Options
               </p>
             </div>
             <div className="border border-solid border-black border-opacity-10 hidden md:grid"></div>
             <div className="mx-auto pb-5">
-              <h3 className="border border-solid border-black border-opacity-10 rounded-[50%] md:w-[74px] md:h-[74px] h-9 w-9   text-center grid items-center text-base md:text-[32px] mx-auto">
+              <h3 className="border bg-white border-solid border-black border-opacity-10 rounded-[50%] md:w-[74px] md:h-[74px] h-9 w-9   text-center grid items-center text-base md:text-[32px] mx-auto">
                 3
               </h3>
-              <p className="mx-auto w-44 md:w-52  text-center md:font-bold md:text-2xl text-[14px] pt-3 md:pt-8">
+              <p className="mx-auto w-44 md:w-52  text-center  md:text-2xl text-[14px] pt-3 md:pt-8">
                 Customize Scene and Character
               </p>
             </div>
             <div className="mx-auto pb-5">
-              <h3 className="border border-solid border-black border-opacity-10 rounded-[50%] md:w-[74px] md:h-[74px] h-9 w-9  text-center grid items-center text-base md:text-[32px] mx-auto ">
+              <h3 className="border bg-white border-solid border-black border-opacity-10 rounded-[50%] md:w-[74px] md:h-[74px] h-9 w-9  text-center grid items-center text-base md:text-[32px] mx-auto ">
                 4
               </h3>
-              <p className="mx-auto w-44 md:w-52  text-center md:font-bold md:text-2xl text-[14px] pt-3 md:pt-8">
-                Customize Scene and Character
+              <p className="mx-auto w-44 md:w-52  text-center  md:text-2xl text-[14px] pt-3 md:pt-8">
+                Download/Share Your Podcast{' '}
               </p>
             </div>
           </div>
-          <div className="mb-10 w-[10.785rem] pt-16 md:pt-0 mx-auto"></div>
         </div>
       </section>
-      <section className="bg-[#fbfbfb]  container mx-auto">
-        <h3 className="text-[292d32] pt-20  text-center font-bold text-xl pb-10 md:pb-[72px] md:text-[40px]">
-          What people are saying
-        </h3>
-        <div className="md:flex flex-wrap gap-12 justify-around pb-[40px] md:px-6">
-          <div className=" px-8 bg-white w-[261px] mx-auto md:w-[378px]  mb-12 md:mb-0 md:px-8 md:h-52 h-36 grid items-center relative rounded-lg md:rounded-2xl  md:pb-0">
-            <div className=" h-12 w-12 md:h-[69] md:w-[69] absolute top-0 left-[50%] translate-x-[-50%] translate-y-[-50%] ">
-              <img src={MarkImg} alt="Mark Essien" className="w-full h-full" />
+      <section className="bg-sec-700 text-white mx-auto pb-5">
+        <div>
+          <h3 className="text-[292d32] pt-20  text-center font-bold text-xl pb-10 md:pb-[72px] md:text-[40px]">
+            What people are saying
+          </h3>
+          <div className="grid md:grid-cols-3 flex-wrap gap-12 justify-around pb-[40px] md:px-6">
+            <div className=" px-8 bg-white w-[261px] mx-auto md:w-[378px]  mb-12 md:mb-0 md:px-8 md:h-52 h-36 grid items-center relative rounded-lg md:rounded-2xl  md:pb-0">
+              <div className=" h-12 w-12 md:h-[69] md:w-[69] absolute top-0 left-[50%] translate-x-[-50%] translate-y-[-50%] ">
+                <img src={MarkImg} alt="Mark Essien" className="w-full h-full" />
+              </div>
+              <img src={Quotes} alt="" className="pt-[34px] md:pt-12 mx-auto" />
+              <p className="text-center text-black opacity-60 text-xs md:text-base italic">
+                Voxclips is the ideal app for intriguing my listeners. It has helped me gain more
+                views and engagements and I love it
+              </p>
+              <h5 className="text-center pb-6 text-[11px] md:text-base ">Mark Essien</h5>
             </div>
-            <img src={Quotes} alt="" className="pt-[34px] md:pt-12 mx-auto" />
-            <p className="text-center text-black opacity-60 text-xs md:text-base italic">
-              Voxclips is the ideal app for intriguing my listeners. It has helped me gain more
-              views and engagements and I love it
-            </p>
-            <h5 className="text-center pb-6 text-[11px] md:text-base ">Mark Essien</h5>
-          </div>
-          <div className=" px-8 bg-white md:w-[378px]  w-[261px] mx-auto mb-12 md:mb-0 md:px-8 md:h-52 h-36 grid items-center relative rounded-lg md:rounded-2xl  md:pb-0">
-            <div className=" h-12 w-12 md:h-[69] md:w-[69] absolute top-0 left-[50%] translate-x-[-50%] translate-y-[-50%] ">
-              <img src={LavyImg} alt="Mark Essien" className="w-full h-full" />
+            <div className=" px-8 bg-white md:w-[378px]  w-[261px] mx-auto mb-12 md:mb-0 md:px-8 md:h-52 h-36 grid items-center relative rounded-lg md:rounded-2xl  md:pb-0">
+              <div className=" h-12 w-12 md:h-[69] md:w-[69] absolute top-0 left-[50%] translate-x-[-50%] translate-y-[-50%] ">
+                <img src={LavyImg} alt="Mark Essien" className="w-full h-full" />
+              </div>
+              <img src={Quotes} alt="" className="pt-[34px] md:pt-12 mx-auto" />
+              <p className="text-center text-black opacity-60 text-xs md:text-base w-48 mx-auto mb-2 italic">
+                I stumbled on this app by accident and to be honest, this has been the best mistake
+                of my life
+              </p>
+              <h5 className="text-center pb-6 text-[11px] md:text-base ">Lavy Engine</h5>
             </div>
-            <img src={Quotes} alt="" className="pt-[34px] md:pt-12 mx-auto" />
-            <p className="text-center text-black opacity-60 text-xs md:text-base w-48 mx-auto mb-2 italic">
-              I stumbled on this app by accident and to be honest, this has been the best mistake of
-              my life
-            </p>
-            <h5 className="text-center pb-6 text-[11px] md:text-base ">Lavy Engine</h5>
-          </div>
-          <div className=" px-8 bg-white w-[261px] md:w-[378px]  mx-auto mb-12 md:mb-0 md:px-8 md:h-52 h-36 grid items-center relative rounded-lg md:rounded-2xl md:pb-0">
-            <div className=" h-12 w-12 md:h-[69] md:w-[69] absolute top-0 left-[50%] translate-x-[-50%] translate-y-[-50%] ">
-              <img src={NonsoImg} alt="Mark Essien" className="w-full h-full" />
+            <div className=" px-8 bg-white w-[261px] md:w-[378px]  mx-auto mb-12 md:mb-0 md:px-8 md:h-52 h-36 grid items-center relative rounded-lg md:rounded-2xl md:pb-0">
+              <div className=" h-12 w-12 md:h-[69] md:w-[69] absolute top-0 left-[50%] translate-x-[-50%] translate-y-[-50%] ">
+                <img src={NonsoImg} alt="Mark Essien" className="w-full h-full" />
+              </div>
+              <img src={Quotes} alt="" className="pt-[34px] md:pt-12 mx-auto" />
+              <p className="text-center text-black opacity-60 text-xs md:text-base italic">
+                Simple, Straightforward and works Nahh. To good to be true
+              </p>
+              <h5 className="text-center pb-6 text-[11px] md:text-base ">Nonso</h5>
             </div>
-            <img src={Quotes} alt="" className="pt-[34px] md:pt-12 mx-auto" />
-            <p className="text-center text-black opacity-60 text-xs md:text-base italic">
-              Simple, Straightforward and works Nahh. To good to be true
-            </p>
-            <h5 className="text-center pb-6 text-[11px] md:text-base ">Nonso</h5>
           </div>
         </div>
       </section>{' '}
@@ -212,6 +216,9 @@ const Home = () => {
           </div>
         </div>
       </section>
+      <Freestyle />
+      <Transform />
+      <StartCreating />
     </Layout>
   );
 };
