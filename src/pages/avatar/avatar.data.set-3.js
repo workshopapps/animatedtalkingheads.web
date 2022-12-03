2fcf96f298c698329804225072fece70c818a875
Daniel Tolani <mumytosin2016@gmail.com>
2022-12-03 08:40:36 +0100

    feat(FRO-85): fixed alert on sign up

---
+++ src/pages/avatar/avatar.data.set-3.js
@@ -0,0 +1,77 @@
+import avatar1 from '../../assets/avatars/set-3/Avatars-01.png';
+import avatar2 from '../../assets/avatars/set-3/Avatars-02.png';
+import avatar3 from '../../assets/avatars/set-3/Avatars-03.png';
+import avatar4 from '../../assets/avatars/set-3/Avatars-04.png';
+import avatar5 from '../../assets/avatars/set-3/Avatars-05.png';
+import avatar6 from '../../assets/avatars/set-3/Avatars-06.png';
+import avatar7 from '../../assets/avatars/set-3/Avatars-07.png';
+import avatar8 from '../../assets/avatars/set-3/Avatars-08.png';
+import avatar9 from '../../assets/avatars/set-3/Avatars-09.png';
+import avatar10 from '../../assets/avatars/set-3/Avatars-10.png';
+import avatar11 from '../../assets/avatars/set-3/Avatars-11.png';
+import avatar12 from '../../assets/avatars/set-3/Avatars-12.png';
+import avatar13 from '../../assets/avatars/set-3/Avatars-13.png';
+import avatar14 from '../../assets/avatars/set-3/Avatars-14.png';
+
+
+const AVATAR_SET_3 = [
+  {
+    id: 1,
+    image: avatar1
+  },
+  {
+    id: 2,
+    image: avatar2
+  },
+  {
+    id: 3,
+    image: avatar3
+  },
+  {
+    id: 4,
+    image: avatar4
+  },
+  {
+    id: 5,
+    image: avatar5
+  },
+  {
+    id: 6,
+    image: avatar6
+  },
+  {
+    id: 7,
+    image: avatar7
+  },
+  {
+    id: 8,
+    image: avatar8
+  },
+  {
+    id: 9,
+    image: avatar9
+  },
+  {
+    id: 10,
+    image: avatar10
+  },
+  {
+    id: 11,
+    image: avatar11
+  },
+  {
+    id: 12,
+    image: avatar12
+  },
+  {
+    id: 13,
+    image: avatar13
+  },
+  {
+    id: 14,
+    image: avatar14
+  }
+];
+
+
+export default AVATAR_SET_3;
