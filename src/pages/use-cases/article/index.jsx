99f010b98c1712bfe358289d2430908039ea51bf
Bitee-cd <Calebduniya45@gmail.com>
2022-12-03 14:31:39 +0100

    changes made to footer,uses cases and pricing

--- src/pages/use-cases/article/index.jsx
+++ src/pages/use-cases/article/index.jsx
@@ -27,7 +27,7 @@ const UseCaseArticle = () => {
           <div className={`mt-10 flex justify-between`}>
             <button
               onClick={() => navigate(-1)}
-              className=" text-blue-600  border rounded-lg border-blue-600 px-4 py-2 md:px-7 md:py-3 w-[150px]">
+              className=" hover:border-sec-600 focus:bg-white  hover:text-sec-600 text-blue-600  border rounded-lg border-blue-600 px-4 py-2 md:px-7 md:py-3">
               <p>Back</p>
             </button>
             <Link to="/podcast/upload">
