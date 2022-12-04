9259af2d14bca9d406da0bba04ec86004b09e41f
anita-ay <ajalaanita690@gmail.com>
2022-12-04 18:15:21 +0100

    fix

--- src/pages/podcast/customization/index.jsx
+++ src/pages/podcast/customization/index.jsx
@@ -41,7 +41,7 @@ const CustomizeAudio = () => {
     const timer = setInterval(() => {
       setProgress((oldProgress) => {
         if (oldProgress === 100) {
-          return 100;
+          return 0;
         }
         const diff = Math.random() * 10;
         return Math.min(oldProgress + diff, 100);
