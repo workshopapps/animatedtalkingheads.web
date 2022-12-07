1732f86db94acd5fe1561119708a0fd0bade47c8
bobbycrooz <idrisloove@gmail.com>
2022-12-07 15:40:25 -0600

    .

--- src/pages/forgot-password/index.jsx
+++ src/pages/forgot-password/index.jsx
@@ -1,6 +1,6 @@
 import Layout from '../../components/UI/Layout';
 import './fogot-password.scss';
-// import axios from 'axios';
+import axios from 'axios';
 import { useState } from 'react';
 
 const ForgotPassword = () => {
@@ -12,23 +12,27 @@ const ForgotPassword = () => {
 
     seIsLoading(true);
     console.log(userEmail);
-    // const url = 'https://api.voxlips.hng.tech/podcasts/upload/';
-    // const config = {
-    //   mode: 'cors',
-    //   headers: {
-    //     'Content-Type': 'application/json'
-    //   }
-    // };
+    const url = 'https://api.voxclips.hng.tech/rauth/forgotpassword';
+    const config = {
+      mode: 'cors',
+      headers: {
+        'Content-Type': 'application/json'
+      }
+    };
 
-    // try {
-    //   const resp = await axios.post(url, userEmail, config);
-    // } catch (error) {
-    //   console.log(error);
-    // }
-    setTimeout(() => {
-      window.alert('password reset link sent, check your email');
+    try {
+      seIsLoading(true);
+      const resp = await axios.get(url, userEmail, config);
+      console.log(resp);
       seIsLoading(false);
-    }, 3000);
+      window.alert('password reset link sent, check your email');
+    } catch (error) {
+      console.log(error);
+    }
+    // setTimeout(() => {
+    //   window.alert('password reset link sent, check your email');
+    //   seIsLoading(false);
+    // }, 3000);
   }
 
   function getUserEmail(e) {
@@ -67,9 +71,11 @@ const ForgotPassword = () => {
 
             <button
               type="submit"
-              className="submit_button bg-pri-700 h-[50px] centered rounded-md w-full py-4 px-6 text-white">
+              disabled={isLoading && true}
+              className="submit_button disabled:cursor-none bg-pri-700 h-[50px] centered rounded-md w-full py-4 px-6 text-white">
               {isLoading ? <span className="spinner-roller m-4"></span> : ' Send Reset Link'}
             </button>
+            <div className="text-red-400 flex text-center">there was an erro here</div>
           </form>
 
           <div className="w-full centered">
