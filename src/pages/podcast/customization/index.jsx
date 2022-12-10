b47ae7714c43a0b911ccf42d36b86cd9c42e1570
Babajide Akinseye <bakinseye247@gmail.com>
2022-12-10 23:00:59 +0100

    modified render page

--- src/pages/podcast/customization/index.jsx
+++ src/pages/podcast/customization/index.jsx
@@ -1,9 +1,11 @@
 import React, { useState } from 'react';
+import { useMutation, useQuery } from '@tanstack/react-query';
 import Layout from '../../../components/UI/Layout';
 import { Text } from '../../../components/UI/Text';
 import { Modal } from '../../../components/UI/Modal/Modal';
 import caretRight from '../../../assets/icons/carretRight.svg';
 import styles from './styles.module.scss';
+
 // import SignUpSection from '../../sign-up/SignUp';
 
 import { Link } from 'react-router-dom';
@@ -19,9 +21,9 @@ import AudioWidget from './components/AudioWidget';
 
 import store from '../../../store/store.js';
 import { setAvatar } from '../../../store/actions/customizeVideoActions';
-import LinearProgress from '@mui/material/LinearProgress';
 import axios from 'axios';
 import AuthWrapper from '../../../components/UI/Auth/AuthWrapper';
+import PropagateLoader from 'react-spinners/PropagateLoader';
 // import { useMutation, useQuery } from '@tanstack/react-query';
 
 const CustomizeAudio = () => {
@@ -29,6 +31,7 @@ const CustomizeAudio = () => {
   // MODAL
   const [modalOpen, setModalOpen] = useState(false);
   const [error, setError] = useState(false);
+  const [status, setStatus] = useState(false);
 
   const showModal = () => {
     setModalOpen(true);
@@ -37,26 +40,6 @@ const CustomizeAudio = () => {
   const hideModal = () => {
     setModalOpen(false);
   };
-  // MODAL
-
-  // PROGRESS BAR
-  const [progress, setProgress] = useState(0);
-
-  React.useEffect(() => {
-    const timer = setInterval(() => {
-      setProgress((oldProgress) => {
-        if (oldProgress === 100) {
-          return 0;
-        }
-        const diff = Math.random() * 10;
-        return Math.min(oldProgress + diff, 100);
-      });
-    }, 500);
-
-    return () => {
-      clearInterval(timer);
-    };
-  }, []);
 
   function setAV() {
     console.log('ca;;ing');
@@ -100,38 +83,91 @@ const CustomizeAudio = () => {
   // };
 
   // generate from api
-  const get_status = () => {
-    if (store.getState().cartReducer.podcast_audio.user_id) {
-      const base_url = 'https://api.voxclips.hng.tech/podcasts/';
-      console.log(store.getState().cartReducer.podcast_audio);
-      const user_id = store.getState().cartReducer.podcast_audio.user_id;
-      const podcast_id = store.getState().cartReducer.podcast_audio.id;
-      const url = `${base_url}${podcast_id}/generate-video`;
-      const headers = { user_id: user_id };
-      const data = {
-        audio_path: store.getState().cartReducer.podcast_audio.file_path,
-        audio_url: store.getState().cartReducer.podcast_audio.file_url,
-        bg_path: '02'
-      };
-      console.log(data, user_id, podcast_id, url);
-      axios
-        .post(url, data, { headers: headers })
-        .then((data) => {
-          console.log(data.data.status);
-          if (data.data.status === 'PENDING') {
-            showModal();
-            console.log(data.data);
-            store.dispatch({ type: 'ADD_PODCAST_VIDEO', payload: data.data });
-          } else {
-            hideModal();
-          }
-          // data.data.status === 'PENDING' ? console.log(data.data) : hideModal();
-        })
-        .catch((err) => {
-          console.log(err);
-        });
-    } else setError('Please upload an audio file');
+
+  const bearerToken = localStorage.getItem('token');
+  const podcast_id = store.getState().cartReducer.podcast_audio._id;
+
+  const postData = async () => {
+    //console.log()
+    const base_url = 'https://api.voxclips.hng.tech/podcasts/';
+    const url = `${base_url}${podcast_id}/generate-video`;
+    console.log({ url });
+    const headers = { Authorization: `Bearer ${bearerToken}` };
+    const data = {
+      bg_path: '01',
+      avater: {
+        a: '03',
+        b: '01'
+      }
+    };
+    return await axios.post(url, data, { headers: headers });
   };
+
+  const handleClick = async () => {
+    // .then((data) => {
+    //   if (data.data.status === 'PENDING') {
+    //     setStatus(true);
+    //     showModal();
+    //     store.dispatch({ type: 'ADD_PODCAST_VIDEO', payload: data.data });
+    //   } else {
+    //     hideModal();
+    try {
+      showModal();
+      const response = await postData();
+      store.dispatch({ type: 'ADD_PODCAST_VIDEO', payload: response.data });
+    } catch (err) {
+      console.log({ err });
+    }
+  };
+
+  const getStatus = async () => {
+    const podcastID = store.getState().cartReducer.podcast_video._id;
+    const url = `https://api.voxclips.hng.tech/animated-videos/${podcastID}`;
+
+    return await axios.get(url, {
+      headers: {
+        Authorization: `Bearer ${bearerToken}`
+      }
+    });
+  };
+
+  // const getStatus = () => {
+  //   const podcastID = store.getState().cartReducer.podcast_audio._id;
+  //   const url = `https://api.voxclips.hng.tech/animated-videos/ ${podcastID}`;
+
+  //   return axios
+  //     .get(url, {
+  //       headers: {
+  //         Authorization: `Bearer ${bearerToken}`
+  //       }
+  //     })
+  //     .then((data) => {
+  //       if (data.data.status === 'COMPLETED' || data.data.status === 'ERROR') {
+  //         setStatus(false);
+  //       }
+  //       console.log('SUB request', data.data.status);
+  //     })
+  //     .catch((err) => {
+  //       console.log(err);
+  //     });
+  // };
+
+  const isConclusiveData = (response) => {
+    const conclusiveStatuses = ['COMPLETED', 'ERROR'];
+    return response && conclusiveStatuses.includes(response.data.status);
+  };
+
+  // const stopPolling = () => {
+  //   setStatus(false);
+  //   return false;
+  // };
+
+  const { isLoading, data, isError, isFetching } = useQuery(['podcast'], getStatus, {
+    refetchInterval: (response) => {
+      return isConclusiveData(response) ? false : 10000;
+    }
+    //(response) => (isConclusiveData(response) ? stopPolling() : 100000)
+  });
 
   return (
     <Layout>
@@ -198,7 +234,11 @@ const CustomizeAudio = () => {
             {/* pafe info */}
 
             <div className="customization-center  my-4 w-full space-y-3">
-              <Text w={'md'} type={'text4'} cap className={'text-[#666666] text-center md:text-left'}>
+              <Text
+                w={'md'}
+                type={'text4'}
+                cap
+                className={'text-[#666666] text-center md:text-left'}>
                 Video Selection Preview
               </Text>
 
@@ -208,13 +248,13 @@ const CustomizeAudio = () => {
             {/* current podcast's audio player widget */}
             <AudioWidget />
             {/* current podcast's audio player widget  end*/}
-
-            <div className="customization-center-timestamp w-full my-6 space-y-3"></div>
+            {/* 
+          <div className="customization-center-timestamp w-full my-6 space-y-3"></div> */}
           </main>
 
           {error && <p className="text-red-600 text-center">{error}</p>}
           <div className="centered w-full my-[5%]">
-            <Button label={'render video'} onClick={get_status}>
+            <Button label={'render video'} onClick={handleClick}>
               Render video
             </Button>
           </div>
@@ -223,8 +263,13 @@ const CustomizeAudio = () => {
           <Modal onClose={hideModal}>
             <div className={styles.progressBarBox}>
               <h5>Your video is rendering...</h5>
+
               <div className={styles.progressBar}>
-                <LinearProgress color="success" variant="determinate" value={progress} />
+                <PropagateLoader color="hsla(214, 98%, 47%, 1)" />
+                <p className="text-[12px] pt-12">
+                  This may take a while. You will get an email notification when it is done
+                  rendering.
+                </p>
               </div>
               <button onClick={hideModal}>Cancel</button>
             </div>
