deac1769527ccc9674fb17e5238c15a9a38995f5
Bitee-cd <Calebduniya45@gmail.com>
2022-12-13 20:53:47 +0100

    profile drop down menu edits

--- src/components/UI/TopNavbar/index.jsx
+++ src/components/UI/TopNavbar/index.jsx
@@ -1,19 +1,16 @@
 import React from 'react';
-import { Link, useLocation, useNavigate } from 'react-router-dom';
+import { Link, useLocation } from 'react-router-dom';
 import { useState, useEffect } from 'react';
-import Hamburger from 'hamburger-react'
+import Hamburger from 'hamburger-react';
 import { Button } from '../Button';
 import styles from './styles.module.css';
 import headerLogo from './../../../assets/icons/header_logo.svg';
 import { links, linksMobile, routes } from '../../../libs/links';
 import { BiChevronDown } from 'react-icons/bi';
 import { UserAuth } from '../../../context/AuthContext';
-import { motion } from "framer-motion";
+import { motion } from 'framer-motion';
 import { menuAnimate } from './animation';
-import { toast } from 'react-toastify';
-import UserProfile from '../UserProfile';
-import Notification from '../Notificaton';
-
+import NavSign from '../UserProfile';
 
 // const miniLinks = [
 //   { name: 'Support', link: '#' },
@@ -24,15 +21,13 @@ import Notification from '../Notificaton';
 const TopNavbar = () => {
   const [show, setShow] = React.useState(false);
   const location = useLocation();
-  const navigate = useNavigate();
   const pathname = location.pathname.split('/')[1];
-  const { user, logOut, setUserToken, setUser } = UserAuth();
+  const { user } = UserAuth();
   const close = () => {
     setShow(false);
   };
 
   const [signInPath, setSignInPath] = useState(false);
-
 
   // const currentPath = '/sign-in'
   // console.log(location.pathname)
@@ -42,28 +37,9 @@ const TopNavbar = () => {
     }
   }, [signInPath]);
 
-  const handleSignOut = async () => {
-    try {
-      await logOut();
-      setTimeout(() => {
-        localStorage.removeItem('token');
-        setUserToken('')
-
-      }, 1000);
-      toast.success("Sign out successful!", {
-        position: toast.POSITION.BOTTOM_RIGHT
-      })
-      setUser(null)
-      navigate('/sign-in');
-    } catch (error) {
-      console.log(error);
-    }
-  };
-
-
   return (
     <div className={styles.nav}>
-      <div className={`h-full flex justify-between items-center px-3 lg:px-20`}>
+      <div className={`container mx-auto h-full flex justify-between items-center w-[90%]`}>
         <div className="flex">
           <Link to="/">
             <img src={headerLogo} alt="home" />
@@ -72,10 +48,11 @@ const TopNavbar = () => {
         <div className={`${styles.links} hidden items-center  lg:flex lg:grow justify-between`}>
           {links.map((link, index) => (
             <Link
-              className={`${`/${pathname}` === link.link || `/${pathname}/1` === link.link
-                ? `${styles.active}`
-                : ''
-                } block`}
+              className={`${
+                `/${pathname}` === link.link || `/${pathname}/1` === link.link
+                  ? `${styles.active}`
+                  : ''
+              } block`}
               key={index}
               to={link.link}>
               {link.name}
@@ -83,7 +60,6 @@ const TopNavbar = () => {
           ))}
 
           <div className={`${styles.dropdown} ${styles.links} px-1 space-x-1 relative middle `}>
-
             <a
               className={
                 pathname === routes.avatars || pathname === routes.scenery ? `${styles.active}` : ''
@@ -107,46 +83,37 @@ const TopNavbar = () => {
           <Link to={routes.contact}>Contact us</Link>
         </div>
         <div className={`hidden lg:flex`}>
+          <Link to={routes.podcastUpload}>
+            <Button label={'Create Video'}>Create Video</Button>
+          </Link>
+
           {/* { user && (<Link onClick={handleSignOut} to={routes.signIn} style={{ color: '#2563EB', alignSelf: 'center', marginRight:'1.875rem' }} > Sign out</Link>)} */}
           {user ? (
-            <button
-              className="hover:border-sec-600 focus:bg-white  hover:text-sec-600 text-blue-600  border rounded-lg border-blue-600 px-4 py-2 md:px-7"
-              style={{
-                alignSelf: 'center',
-                marginRight: '1rem'
-              }}
-              onClick={handleSignOut}>
-              Sign out {/* {user?.displayName} */}
-            </button>
+            <NavSign />
           ) : (
             <Link
               to={routes.signIn}
               className="hover:border-sec-600 focus:bg-white  hover:text-sec-600 text-blue-600  border rounded-lg border-blue-600 px-4 py-2 md:px-7"
               style={{
                 alignSelf: 'center',
-                marginRight: '1rem'
+                marginLeft: '1rem'
               }}>
               Sign In
             </Link>
           )}
 
-          <Link to={routes.podcastUpload}>
-            <Button label={'Create Video'}>Create Video</Button>
-          </Link>
-          <UserProfile />
           {/* <Notification /> */}
         </div>
 
-        <div className='lg:hidden'>
+        <div className="lg:hidden flex gap-5 items-center">
+          {user && <NavSign />}
           <Hamburger size={28} toggled={show} toggle={() => setShow(!show)} />
         </div>
-
       </div>
 
-
       <motion.div
-        className={`${styles.menu} ${show ? "show" : 'hidden'} px-3 py-10 lg:hidden`}
-        animate={show ? "enter" : "exit"}
+        className={`${styles.menu} ${show ? 'show' : 'hidden'} px-3 py-10 lg:hidden`}
+        animate={show ? 'enter' : 'exit'}
         variants={menuAnimate}>
         <div className={``}>
           <div className={`${styles.links}`}>
@@ -171,30 +138,21 @@ const TopNavbar = () => {
 
           <div className="flex w-full justify-center mt-6 items-center">
             <Link onClick={close} className={styles.createVideo} to={routes.podcastUpload}>
-              <button className='text-[#fefefe]'> Create Video </button>
+              <button className="text-[#fefefe]"> Create Video </button>
             </Link>
           </div>
 
           <div className="flex w-full justify-center items-center">
-            {user ? (
-              <button
-                className="w-[240px] h-[58px] hover:border-sec-600 focus:bg-white  hover:text-sec-600 text-blue-600  border rounded-lg border-blue-600 px-4 py-2 md:px-7 md:py-3"
-                onClick={handleSignOut}>
-                Sign out {/* {user?.displayName} */}
-              </button>
-            ) : (
-              <Link
-                to={routes.signIn}>
+            {!user && (
+              <Link to={routes.signIn}>
                 <button className="w-[240px] h-[58px] hover:border-sec-600 focus:bg-white  hover:text-sec-600 text-blue-600  border rounded-lg border-blue-600 px-4 py-2 md:px-7 md:py-3">
                   Sign In
                 </button>
               </Link>
             )}
           </div>
-
         </div>
       </motion.div>
-
     </div>
   );
 };
