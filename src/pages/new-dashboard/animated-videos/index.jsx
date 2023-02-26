b8370410eb302590a3b5fa0c87f93ae276a12f56
Caleb Bitiyong Duniya <calebduniya45@gmail.com>
2023-02-26 17:31:30 +0100

    Created new Dashboard Layout (#273)
    * changed url paths and name of files
    
    * Implementation of Inprogress Page
    
    * Creating of dashboard layout

---
+++ src/pages/new-dashboard/animated-videos/index.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import DashboardLayout from '../../../components/UI/DashboardLayout';
+
+const AnimatedVideos = () => {
+  return (
+    <DashboardLayout>
+      <div className="bg-red">This is the animated Videos Page</div>
+    </DashboardLayout>
+  );
+};
+
+export default AnimatedVideos;
