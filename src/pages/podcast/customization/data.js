711ba68c1d71d0a6be5fd0ef59bd7a129c393fe9
Joshua Evuetapha <Evuetaphajoshua@gmail.com>
2023-02-26 20:20:13 +0100

    Added more backgrounds

--- src/pages/podcast/customization/data.js
+++ src/pages/podcast/customization/data.js
@@ -7,6 +7,19 @@ import avatar06 from './../../../assets/avatars/supported/06.png';
 
 import bg01 from './../../../assets/avatars/bg-supported/01.png';
 import bg02 from './../../../assets/avatars/bg-supported/02.png';
+import bg03 from './../../../assets/avatars/bg-supported/03.png';
+import bg04 from './../../../assets/avatars/bg-supported/04.png';
+import bg05 from './../../../assets/avatars/bg-supported/05.png';
+import bg06 from './../../../assets/avatars/bg-supported/06.png';
+import bg07 from './../../../assets/avatars/bg-supported/07.png';
+import bg08 from './../../../assets/avatars/bg-supported/08.png';
+import bg09 from './../../../assets/avatars/bg-supported/09.png';
+import bg10 from './../../../assets/avatars/bg-supported/10.png';
+import bg11 from './../../../assets/avatars/bg-supported/11.png';
+import bg12 from './../../../assets/avatars/bg-supported/12.png';
+import bg13 from './../../../assets/avatars/bg-supported/13.png';
+import bg14 from './../../../assets/avatars/bg-supported/14.png';
+import bg15 from './../../../assets/avatars/bg-supported/15.png';
 
 export const formatId = (id, position) => {
   let result = Number(id) + position;
@@ -27,5 +40,18 @@ export const avatars = [
 
 export const scenes = [
   { id: '01', image: bg01 },
-  { id: '02', image: bg02 }
+  { id: '02', image: bg02 },
+  { id: '03', image: bg03 },
+  { id: '04', image: bg04 },
+  { id: '05', image: bg05 },
+  { id: '06', image: bg06 },
+  { id: '07', image: bg07 },
+  { id: '08', image: bg08 },
+  { id: '09', image: bg09 },
+  { id: '10', image: bg10 },
+  { id: '11', image: bg11 },
+  { id: '12', image: bg12 },
+  { id: '13', image: bg13 },
+  { id: '14', image: bg14 },
+  { id: '15', image: bg15 }
 ];
